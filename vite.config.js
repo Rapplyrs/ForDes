ed89f50371fb2dee5d0e29a59ef9065520f4bd1f
Rovaldy Applyrs <rapplyrs@Rovaldys-MacBook-Pro.local>
2026-02-07 22:24:49 -0700

    minor

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/valentine-app/', // 👈 repo name goes here
+  base: '/ForDes/', // 👈 repo name goes here
 })
